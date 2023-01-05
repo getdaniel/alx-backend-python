ed538e077d5003c21e2b5b22c4f704d8231e57c5
Me <getdaniel>
2023-01-05 12:02:47 +0300

    list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from typing import List
+""" A type-annotation of sum_list."""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Return sum of list."""
+    return float(sum(input_list))
