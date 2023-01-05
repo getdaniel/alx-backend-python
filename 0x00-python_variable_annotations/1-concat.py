291abbb0a6c836aad09cb49f9a7dd9f801be7332
Me <getdaniel>
2023-01-05 11:28:22 +0300

    concat fun.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A type-annotated function concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns  the concatenation of two strings."""
+    return str1 + str2
