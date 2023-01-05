9ba6dad9d5716df6b77d106c787c302055099186
Me <getdaniel>
2023-01-05 13:39:22 +0300

    first element of a sequence

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from typing import Any, Sequence, Union
+"""The types of the elements of the input are not know"""
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of a sequence if it exists."""
+    if lst:
+        return lst[0]
+    else:
+        return None
